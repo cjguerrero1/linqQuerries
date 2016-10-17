d918257f7a70fe9745e45bfebac50f51794f564b
CJ Guerrero <CJ Guerrero>
2016-10-17 07:36:19 -0500

    initial commit

---
+++ FunctionalProgramming/FunctionalProgramming/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> exampleList = new List<string> { "hello", "my", "hello", "name", "my", "is", "cj" };
+            Querries querrie = new Querries();
+            querrie.RemoveDuplicates(exampleList);
+            Console.ReadKey();
+            List<string> classGrades = new List<string> { "50,95,85,67,100", "63,98,78,86,92", "0,82,94,97,85", "100,90,93,80,89" };//89.40625
+            querrie.FindClassAverage(classGrades);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FunctionalProgramming/FunctionalProgramming/Querries.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    class Querries
+    {
+        public void RemoveDuplicates(List<string>originalList)
+        {
+            var newList = originalList.Distinct();
+            foreach(string item in newList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void FindClassAverage(List<string> classGrades)
+        {
+            List<double> classAverage = new List<double>();
+            foreach (string studentGrades in classGrades)
+            {
+                classAverage.Add(GetStudentAverage(studentGrades));
+            }
+            var finalAverage = classAverage.Average();
+            Console.WriteLine(finalAverage);
+        }
+
+        public double GetStudentAverage(string studentGrades)
+        {
+            var gradesAsInts = studentGrades.Split(',').Select(int.Parse).ToList();
+            int lowestGrade = gradesAsInts.Min();
+            gradesAsInts.Remove(lowestGrade);
+            double studentAverage = gradesAsInts.Average();
+            return studentAverage;
+            
+
+        }
+    }
+}
